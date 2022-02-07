b296dcc01ad7bc4a155ffe4dc23969602fe325a5
BeautyKim <kima2012114@gmail.com>
2022-02-07 09:18:28 +0900

    2022-02-07 09:18

--- java/com/example/demo/controller/TempController.java
+++ java/com/example/demo/controller/TempController.java
@@ -30,14 +30,15 @@ public class TempController {
                            "큐", "sort", "DFS", "그래프",
                           "힙",           "BFS",             "Greedy"};
         String s = "";
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i%5==0) {
+        for(int i = 0; i < arr.length; i++){
+            if(i%5==0){
                 s += "\n";
             }
-            s += i + ":" + arr[i] + "\t";
+            s += arr[i]+"\t";
         }
         System.out.println(s+"\n");
+
+
         System.out.println("Q2. 팀장이 맡은 과제만 출력하세요 ");
         String zzang = "";
         String name = "강민";
@@ -64,45 +65,9 @@ public class TempController {
 
 
 
-        s = "";
-        String ind = "큐";
-        System.out.println("Q3-1. 입력한 과목의 인덱스를 추출하세요 ");
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i].equals(ind)){
-                System.out.println(arr[i]+"의 인덱스 = "+i);
-            }
-        }
-
-
         System.out.println("Q4. 팀원별 과제 수를 출력하세요 ");
         int count = 0;
         String countManager = "";
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int count4 = 0;
-        int[] intArr = new int[5];
-
-        for(int i=0; i<arr.length; i++){
-            if(arr[i].equals(arr[11])){
-                count0++;
-            }if(arr[i].equals(arr[6])){
-                count1++;
-            }if(arr[i].equals(arr[14])){
-                count2++;
-            }if(arr[i].equals(arr[7])){
-                count3++;
-            }if(arr[i].equals(arr[8])){
-                count4++;
-            }
-        }
-
-        System.out.println(arr[0]+"의 카운트는 " + count0);
-        System.out.println(arr[1]+"의 카운트는 " + count1);
-        System.out.println(arr[2]+"의 카운트는 " + count2);
-        System.out.println(arr[3]+"의 카운트는 " + count3);
-        System.out.println(arr[4]+"의 카운트는 " + count4);
 
     }
 
