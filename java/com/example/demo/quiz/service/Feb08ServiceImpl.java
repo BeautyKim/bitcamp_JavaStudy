c72b667c8de93e061554fed3742f3484345b9bee
BeautyKim <kima2012114@gmail.com>
2022-02-09 09:29:32 +0900

    2022-02-09 09:29 과제8 좌석예약 수정

--- java/com/example/demo/quiz/service/Feb08ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb08ServiceImpl.java
@@ -26,36 +26,36 @@ public class Feb08ServiceImpl implements Feb08Service{
 
     @Override
     public void booking(Scanner scanner) {
-            int seat[] = new int[22];
-            int reserved;
-            String s = "";
-            while (true){
-                System.out.println("Q3. 좌석예약\n 자석 예약 시스템 입니다.\n 원하시는 메뉴를 입력해주세요\n 0.종료 1.예약");
-                switch (scanner.next()){
-                    case "0":
-                        System.out.println("예약을 종료하겠습니다.");return;
-                    case "1":
-                        System.out.println("이름");
-                        String name = scanner.next();
-                        System.out.println("현재 예약상태");
-                        for(int i = 0; i<22; i++){
-                            s += i+1+"\t\n";
-                        }
-                        System.out.println(s);
+        int seat[] = new int[22];
+        int reserved;
+        String s = "";
+        while (true){
+            System.out.println("Q3. 좌석예약\n 자석 예약 시스템 입니다.\n 원하시는 메뉴를 입력해주세요\n 0.종료 1.예약");
+            switch (scanner.next()){
+                case "0":
+                    System.out.println("예약을 종료하겠습니다.");return;
+                case "1":
+                    System.out.println("이름");
+                    String name = scanner.next();
+                    System.out.println("현재 예약상태");
+                    for(int i = 0; i<22; i++){
+                        s += i+1+"\t";
+                    }
+                    System.out.println(s);
 
-                        for (int i : seat)
-                            System.out.print(i+"\t");
-                        System.out.println("좌석선택");
-                        reserved = scanner.nextInt();
+                    for (int i : seat)
+                        System.out.print(i+"\t");
+                    System.out.println("\n좌석선택");
+                    reserved = scanner.nextInt();
 
-                        seat[reserved - 1]=1;
-                        System.out.println(name+"님 예약되었습니다.");
-                        break;
-                    default:
-                        System.out.println("메뉴를 다시 선택해주세요");
-                }
+                    seat[reserved - 1]=1;
+                    System.out.println(name+"님 예약되었습니다.");
+                    break;
+                default:
+                    System.out.println("메뉴를 다시 선택해주세요");
             }
         }
+            }
 
     @Override
     public void bank(Scanner scanner) {
